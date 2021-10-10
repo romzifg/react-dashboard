0d88a9ea91c696406ab379a1aff90e7a2b85549c
Romzi Farhan <romzifg@gmail.com>
2021-10-10 21:09:33 +0700

    add large widget

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -2,7 +2,82 @@ import React from "react";
 import "./widgetlg.css";
 
 function WidgetLg() {
-  return <div className="widgetLg">Large</div>;
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
+  return (
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://picsum.photos/200/300"
+              alt="img"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.001</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://picsum.photos/200/300"
+              alt="img"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.001</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://picsum.photos/200/300"
+              alt="img"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.001</td>
+          <td className="widgetLgStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://picsum.photos/200/300"
+              alt="img"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">2 Jun 2021</td>
+          <td className="widgetLgAmount">$122.001</td>
+          <td className="widgetLgStatus">
+            <Button type="Declined" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default WidgetLg;
